ff2ac9d7e494e3e56cf67ad0162b423a7e63bc94
20BCA17 <83696439+20BCA17@users.noreply.github.com>
2024-03-23 13:00:20 +0530

    Add files via upload

---
+++ landwin-project/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './Component/Header'
+import Block from './Component/Block'
+import SocialMedia from './Component/SocialMedia'
+import Mid from './Component/Mid'
+import Footer from './Component/Footer'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Block />
+      
+      <SocialMedia />
+      <Mid />
+  <Footer />
+    </div>
+  )
+}
+
+export default App
